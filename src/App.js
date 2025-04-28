5512cad56c751be89c7f1dac11403580e4ca1521
Flor3ncia-Andr4d4 <florencicarolina031@gmail.com>
2025-04-27 22:28:09 -0300

    Actividad 7

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; 
+import Testimonial from './Testimonial';
+
+function App() {
+  return (
+    <div className="App">
+      {/*  el componente Testimonial */}
+      <Testimonial />
+    </div>
+  );
+}
+
+export default App;
